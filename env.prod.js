c864e1a31464b6a903515f73d828e20347bf99d6
Alexander Jukov <alex.zhukov@gbooking.ru>
2020-06-16 16:30:56 +0500

    remove redundand file

--- env.prod.js
+++
@@ -1,8 +0,0 @@
-export var CORE_API_ENDPOINT = "https://apiv2.gbooking.ru/rpc";
-export var CRAC_SLOTS_API_ENDPOINT = "http://cracslots.gbooking.ru/rpc";
-export var CRAC_API_ENDPOINT = "http://crac-prod.gbooking.ru/rpc";
-export var CRAC3_API_ENDPOINT = "http://crac-prod3.gbooking.ru/rpc";
-export var OAUTH_OTP_SEND = "https://oauthv2.gbooking.ru/sms/code";
-export var OAUTH_OTP_VERIFY = "https://oauthv2.gbooking.ru/sms/verify";
-export var API_REQUEST_DEBUG = true;
-export var OTP_REQUEST_DEBUG = true;

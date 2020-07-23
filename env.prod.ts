6b071eb0c85dc11894b8380affc129c5bb436586
Alexander Jukov <alex.zhukov@gbooking.ru>
2020-07-23 14:58:42 +0500

    dev init

--- env.prod.ts
+++ env.prod.ts
@@ -1,10 +1,27 @@
-export const CORE_API_ENDPOINT: string = "https://apiv2.gbooking.ru/rpc";
-export const CRAC_SLOTS_API_ENDPOINT: string = "http://cracslots.gbooking.ru/rpc";
+// export const CORE_API_ENDPOINT: string = "https://apiv2.gbooking.ru/rpc";
+// export const CRAC_SLOTS_API_ENDPOINT: string = "http://cracslots.gbooking.ru/rpc";
+// export const CRAC_API_ENDPOINT: string = "http://crac-prod.gbooking.ru/rpc";
+// export const CRAC3_API_ENDPOINT: string = "http://crac-prod3.gbooking.ru/rpc";
+// export const OAUTH_OTP_SEND: string = "https://oauthv2.gbooking.ru/sms/code";
+// export const OAUTH_OTP_VERIFY: string = "https://oauthv2.gbooking.ru/sms/verify";
+// export const OAUTH_OTP_WEBLOGIN: string = "https://oauthv2.gbooking.ru/web_login";
+
+// export const CORE_API_ENDPOINT: string = "http://api.gbooking.test:3000/rpc";
+// export const CRAC_SLOTS_API_ENDPOINT: string = "http://api.gbooking.test:3000/rpc";
+// export const CRAC_API_ENDPOINT: string = "http://crac-prod.gbooking.ru/rpc";
+// export const CRAC3_API_ENDPOINT: string = "http://crac-prod3.gbooking.ru/rpc";
+// export const OAUTH_OTP_SEND: string = "http://api.gbooking.test:3001/sms/code";
+// export const OAUTH_OTP_VERIFY: string = "http://api.gbooking.test:3001/sms/verify";
+// export const OAUTH_OTP_WEBLOGIN: string = "http://api.gbooking.test:3001/web_login";
+
+
+export const CORE_API_ENDPOINT: string = "http://api2.dev.gbooking.ru/rpc";
+export const CRAC_SLOTS_API_ENDPOINT: string = "http://crac-prod3.gbooking.ru/rpc";
 export const CRAC_API_ENDPOINT: string = "http://crac-prod.gbooking.ru/rpc";
 export const CRAC3_API_ENDPOINT: string = "http://crac-prod3.gbooking.ru/rpc";
-export const OAUTH_OTP_SEND: string = "https://oauthv2.gbooking.ru/sms/code";
-export const OAUTH_OTP_VERIFY: string = "https://oauthv2.gbooking.ru/sms/verify";
-export const OAUTH_OTP_WEBLOGIN: string = "https://oauthv2.gbooking.ru/web_login";
+export const OAUTH_OTP_SEND: string = "http://oauth.dev.gbooking.ru/sms/code";
+export const OAUTH_OTP_VERIFY: string = "http://oauth.dev.gbooking.ru/sms/verify";
+export const OAUTH_OTP_WEBLOGIN: string = "http://oauth.dev.gbooking.ru/web_login";
 
-export const API_REQUEST_DEBUG: boolean = true;
-export const OTP_REQUEST_DEBUG: boolean = true;
+export const API_REQUEST_DEBUG: boolean = false;
+export const OTP_REQUEST_DEBUG: boolean = false;

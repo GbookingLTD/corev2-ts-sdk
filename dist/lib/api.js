b3573f923e3f1b4585ce4c8ff0a7df0ac07346c1
Alexander Jukov <alex.zhukov@gbooking.ru>
2020-07-24 10:52:36 +0500

    keep dist in git

---
+++ dist/lib/api.js
@@ -0,0 +1,43 @@
+import { MedMeAPIBusiness } from "./business";
+import { MedMeAPICracSlots } from "./cracSlots";
+import { MedMeAPIClient } from "./client";
+import { MedMeAPIAppointment } from "./appointment";
+import { MedMeAPIOTPAuthorize } from "./otpAuthorize";
+import { MedMedAPIBusinessModel } from "./businessModel";
+/**
+ *
+ */
+var MedMeAPI = /** @class */ (function () {
+    function MedMeAPI() {
+    }
+    /**
+     * Создание бизнес модели для управления данными, полученными из api.
+     * @param business
+     */
+    MedMeAPI.createBusinessModel = function (business) {
+        return new MedMedAPIBusinessModel(business);
+    };
+    /**
+     * Набор методов для доступа к методам API с префиксом "business".
+     */
+    MedMeAPI.business = new MedMeAPIBusiness();
+    /**
+     * Методы для получения слотов расписания.
+     */
+    MedMeAPI.slots = new MedMeAPICracSlots();
+    /**
+     * Методы для создания и/или получения клиента, редактирования данных клиента.
+     */
+    MedMeAPI.client = new MedMeAPIClient();
+    /**
+     * Методы для резервирования, подтверждения, отмены записи, снятия резерва записи, получения записей.
+     */
+    MedMeAPI.appointment = new MedMeAPIAppointment();
+    /**
+     * Методы для OTP авторизации.
+     */
+    MedMeAPI.otpAuthorize = new MedMeAPIOTPAuthorize();
+    return MedMeAPI;
+}());
+export { MedMeAPI };
+export * as GBookingCoreV2 from 'corev2-schemata/langs/typescript/GBookingCoreV2';
---
+++ dist/lib/otpAuthorize.d.ts
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+export interface OTPAuthenticateSendResponse {
+    token: string;
+}
+/**
+ *
+ */
+export interface OTPAuthenticateVerifyResponse {
+    user: string;
+    token: string;
+    auth_user: string;
+    expires: string;
+    askNewPass: boolean;
+}
+/**
+ * Методы OTP авторизации.
+ */
+export declare class MedMeAPIOTPAuthorize {
+    /**
+     * Отправить OTP код на номер телефона.
+     * @param businessId
+     * @param phoneCountry
+     * @param phoneArea
+     * @param phoneNumber
+     */
+    send(businessId: string, phoneCountry: string, phoneArea: string, phoneNumber: string): Promise<OTPAuthenticateSendResponse>;
+    /**
+     * Отправляет запрос на проверку OTP кода.
+     * В ответ возвращает параметры сессии.
+     * @param oauthClientId
+     * @param token
+     * @param code
+     * @param resetPassword
+     */
+    verify(oauthClientId: string, token: string, code: string, resetPassword?: boolean): Promise<OTPAuthenticateVerifyResponse>;
+    /**
+     * Receive users credentials by phone and password
+     * @param phone
+     * @param password
+     */
+    webLogin(phone: string, password: string): Promise<OTPAuthenticateVerifyResponse>;
+}
